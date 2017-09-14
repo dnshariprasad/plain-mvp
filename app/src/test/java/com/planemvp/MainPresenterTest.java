cca0287e2a1a26f28f28e261a035012f48ad5859
Hari <dnshariprasad@gmail.com>
2017-09-14 14:43:08 -0700

    plane mvp step-3 -- add mockito

--- app/src/test/java/com/planemvp/MainPresenterTest.java
+++ app/src/test/java/com/planemvp/MainPresenterTest.java
@@ -6,7 +6,12 @@ import com.planemvp.main.MainRepository;
 import com.planemvp.main.MainView;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -15,66 +20,33 @@ import java.util.List;
 /**
  * Created by Hari on 14/09/17.
  */
-
+@RunWith(MockitoJUnitRunner.class)
 public class MainPresenterTest {
     public static final boolean DATA_EMPTY = true;
     public static final boolean NOT_DATA_EMPTY = false;
+    List<Main> MOCK_DATA = Arrays.asList(new Main("Hari"), new Main("Ravi"), new Main("Pa1"));
+    @Mock
+    MainView mainView;
+    @Mock
+    MainRepository mainRepository;
+    MainPresenter mainPresenter;
+
+    @Before
+    public void setUp() throws Exception {
+        mainPresenter = new MainPresenter(mainView, mainRepository);
+    }
 
     @Test
     public void shouldPassDataToView() {
-        //3 parts to test
-        //given (initial condition)
-        MainView mainView = new MockMainView();
-        MainRepository mainRepository = new MockMainRepository(NOT_DATA_EMPTY);
-
-        //when (action to trigger)
-        MainPresenter mainPresenter = new MainPresenter(mainView, mainRepository);
+        Mockito.when(mainRepository.getData()).thenReturn(MOCK_DATA);
         mainPresenter.loadData();
-
-        //when (action to trigger)
-        Assert.assertEquals(true, ((MockMainView) mainView).displayDataCalled);
+        Mockito.verify(mainView).displayData(MOCK_DATA);
     }
 
     @Test
     public void shouldHandleNoData() {
-        MainView mainView = new MockMainView();
-        MainRepository mainRepository = new MockMainRepository(DATA_EMPTY);
-
-
-        MainPresenter mainPresenter = new MainPresenter(mainView, mainRepository);
+        Mockito.when(mainRepository.getData()).thenReturn(Collections.EMPTY_LIST);
         mainPresenter.loadData();
-
-        Assert.assertEquals(true, ((MockMainView) mainView).displayNoDataCalled);
-    }
-
-    private class MockMainView implements MainView {
-        boolean displayDataCalled;
-        boolean displayNoDataCalled;
-
-        @Override
-        public void displayData(List<Main> mains) {
-            displayDataCalled = true;
-        }
-
-        @Override
-        public void displayNoData() {
-            displayNoDataCalled = true;
-        }
-    }
-
-    private class MockMainRepository implements MainRepository {
-        private boolean mDataFlag;
-
-        MockMainRepository(boolean mDataFlag) {
-            this.mDataFlag = mDataFlag;
-        }
-
-        @Override
-        public List<Main> getData() {
-            if (mDataFlag == NOT_DATA_EMPTY)
-                return Arrays.asList(new Main("Hari"), new Main("Ravi"), new Main("Pa1"));
-            else
-                return Collections.emptyList();
-        }
+        Mockito.verify(mainView).displayNoData();
     }
 }
